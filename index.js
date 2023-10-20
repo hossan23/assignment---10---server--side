a52add1216fb36a37d33d5b53ed9e49f05e7007d
hossan23 <imamhossan101@gmail.com>
2023-10-20 22:10:04 +0600

    server side-env added

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-//imamhossan101
-// IpkmcLYTWyh4MqM0
 
-const uri = 'mongodb+srv://imamhossan101:IpkmcLYTWyh4MqM0@cluster0.fvjw5tg.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fvjw5tg.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,14 +23,88 @@ const client = new MongoClient(uri, {
 
 async function run() {
  try {
-  // Connect the client to the server	(optional starting in v4.7)
   await client.connect();
-  // Send a ping to confirm a successful connection
+
+  const productCollection = client.db('productDB').collection('products');
+
+  const cartCollection = client.db('productDB').collection('cart');
+
+  //   posting product to database
+  app.post('/products', async (req, res) => {
+   const newProduct = req.body;
+   console.log(newProduct);
+   const result = await productCollection.insertOne(newProduct);
+   console.log(result);
+   res.send(result);
+  });
+
+  // posting cart item to database
+  app.post('/myCart', async (req, res) => {
+   const newItem = req.body;
+   console.log(newItem);
+   const result = await cartCollection.insertOne(newItem);
+   console.log(result);
+   res.send(result);
+  });
+  // api for product
+  app.get('/products', async (req, res) => {
+   const result = await productCollection.find().toArray();
+   console.log(result);
+   res.send(result);
+  });
+
+  app.get('/products/:_id', async (req, res) => {
+   const id = req.params._id;
+   const query = { _id: new ObjectId(id) };
+   const result = await productCollection.findOne(query);
+   console.log(result);
+   res.send(result);
+  });
+
+  //   api for cart item
+
+  app.get('/myCart', async (req, res) => {
+   const result = await cartCollection.find().toArray();
+   console.log(result);
+   res.send(result);
+  });
+
+  //   deleting cart item
+
+  app.delete('/myCart/:id', async (req, res) => {
+   const id = req.params.id;
+   const query = { _id: new ObjectId(id) };
+   const result = await cartCollection.deleteOne(query);
+   res.send(result);
+  });
+
+  //   update product
+
+  app.put('/products/:_id', async (req, res) => {
+   const id = req.params._id;
+   const filter = { _id: new ObjectId(id) };
+   const option = { upsert: true };
+   const updatedProduct = req.body;
+
+   const update = {
+    $set: {
+     image: updatedProduct.image,
+     name: updatedProduct.name,
+     brand: updatedProduct.brand,
+     type: updatedProduct.type,
+     price: updatedProduct.price,
+     description: updatedProduct.description,
+     rating: updatedProduct.rating,
+    },
+   };
+   const result = await productCollection.updateOne(filter, update, option);
+   res.send(result);
+  });
+
   await client.db('admin').command({ ping: 1 });
   console.log('Pinged your deployment. You successfully connected to MongoDB!');
  } finally {
-  // Ensures that the client will close when you finish/error
-  await client.close();
+  //   await client.close();
  }
 }
 run().catch(console.dir);
